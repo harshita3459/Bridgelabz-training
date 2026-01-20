523818d01ee8d9f1e2695f93de363ac837515a98
Harshita Agrawal <harshiagrawal3459@gmail.com>
2026-01-20 08:00:15 +0530

    constructor chaining

--- review/Vehicle.java
+++ review/Vehicle.java
@@ -1,5 +1,31 @@
 package review;
 
 public class Vehicle {
-
+  String name;
+	double price;
+	String colour;
+	String ownerName;
+	int speed;
+	
+	Vehicle(String name, double price, String colour, String ownerName){
+		this.name = name;
+		this.price = price;
+		this.colour = colour;
+		this.ownerName = ownerName;
+	}
+	
+	Vehicle(String ownerName){
+		this("Unknown", 5000.0, "Black", "Unknown");
+	}
+	
+	Vehicle(int speed){
+    this("Unknown", 1000.0, "not specified", "Unknown");
+		this.speed = speed;
+	}
+	
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle("Scorpio",123000, "white", "harshita");
+		Vehicle v2 = new Vehicle(23);
+		Vehicle v3 = new Vehicle("vanshika");
+	}
 }
